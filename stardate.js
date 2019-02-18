64d99a9f0bdd542977bde042bba528fe8ee4b22c
Glitch (purple-picture) <none>
2019-02-18 20:01:39 +0000

    🐔👖 Tidied and comments

--- stardate.js
+++ stardate.js
@@ -1,8 +1,9 @@
 // file to convert timestamp to start date
 
-const base=2005
-const stardateYear=58000.0
+const base=2005 //b
+const stardateYear=58000.0 //c
 
+// m is the number of days to start of month
 var getMonthNumber = (date) => {
   var first = new Date(date.getFullYear(), 0, 1);
   var theDay = Math.round(((date - first) / 1000 / 60 / 60 / 24) + .5, 0);
@@ -14,10 +15,12 @@ var isLeapYear = (date) => {
  return (leapYear.getDate() == 29) 
 }
 
+// y is the number of days in year (inculding leap years)
 var getYearNumber = (date) => {
  return isLeapYear(date) ? 366 : 365 
 }
 
+// d is the day of the month
 var stardate = (date) => {
   // c + (1000*(y-b)) + ((1000/n)*(m + d -1))
   var sDate = stardateYear 
